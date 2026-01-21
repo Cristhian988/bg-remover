0c1e6c14dbd8b05f74f85e50c7e18cd96237b81c
Criss <crissfx28@gmail.com>
2026-01-21 13:43:45 -0500

    feat: integrate Clerk authentication

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useClerk, UserButton } from "@clerk/clerk-react";
 
 export const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useClerk();
+
   return (
     <div className="container flex items-center justify-between mx-auto py-3 px-6">
       <Link to="/" className="flex items-center gap-1">
         <img src={assets.logo_icon} alt="logo" className="w-10 sm:w-12" />
         <span className="text-xl sm:text-2xl font-bold">BG Remover</span>
       </Link>
-      <button className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 rounded-full text-sm hover:scale-105 transition-all duration-700 cursor-pointer">
-        Empezar{" "}
-        <img src={assets.arrow_icon} alt="arrow" className="w-3 sm:w-4" />
-      </button>
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={() => openSignIn({})}
+          className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 rounded-full text-sm hover:scale-105 transition-all duration-700 cursor-pointer"
+        >
+          Empezar{" "}
+          <img src={assets.arrow_icon} alt="arrow" className="w-3 sm:w-4" />
+        </button>
+      )}
     </div>
   );
 };
